844d33f9536c16bcbac161ed0a94e2c65438bb5e
Princess Wilson <aurorapwilson1@gmail.com>
2019-05-01 13:13:14 -0400

    migrations added, and tables created

---
+++ models/users.js
@@ -0,0 +1,37 @@
+'use strict';
+export default (sequelize, DataTypes) => {
+  const Users = sequelize.define('Users', {
+    username: {
+			type: DataTypes.STRING,
+            allowNull: false,
+            // hard coding in 'anonymousUser' for folks who want to continue on as a guest without creating an account
+            defaultValue: 'anonymousUser',
+			validate: {
+                //username must be at least 5 character long
+				len: [5]
+			}
+		},
+		password: {
+			type: DataTypes.STRING,
+            allowNull: false,
+            // hard coding anonymousUserPassword for folks who want to coninue on as a guest without creating an account
+            defaultValue: 'anonymousUserPassword',
+			validate: {
+                //password must be at least 8 characters long
+				len: [8]
+			}
+		}
+        
+	});
+
+  Users.associate = function(models) {
+    // associations can be defined here
+    Users.hasMany(modes.Issues, {
+      foreignKey: 'userId',
+      as: 'issues',
+      onDelete: "cascade"
+    });
+
+  };
+  return Users;
+};
